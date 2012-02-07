f7c75f675f7ca9a46acd4a30157def949100e7d4
Hideyuki Shimooka <shimooka@doyouphp.jp>
2012-02-07 14:20:49 +0900

    - added tests for Acme/ResponseListenerBundle

--- src/Acme/ResponseListenerBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Acme/ResponseListenerBundle/Tests/Controller/DefaultControllerTest.php
@@ -6,12 +6,42 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function getExtensionData()
+    {
+        return array(
+            array('', 'text/html', 200),
+            array('html', 'text/html', 200),
+            array('json', 'application/json', 200),
+            array('xml', 'text/xml', 200),
+            array('csv', 'application/octet-stream; name=ResponseListenerBundle.csv', 200),
+            array('invalid', 'text/html', 404),
+        );
+    }
+
+
+    public function testDefault()
     {
         $client = static::createClient();
+        $crawler = $client->request('GET', '/ResponseListenerBundle/');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), 'ステータスコードチェック');
+        $this->assertEquals('text/html', $client->getResponse()->headers->get('Content-Type'), 'Content-Typeチェック');
+        $this->assertTrue($crawler->filter('html:contains("Hello ResponseListenerBundle")')->count() === 1);
+    }
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+    /**
+     * @dataProvider getExtensionData
+     */
+    public function testIndex($format, $content_type, $status_code)
+    {
+        $extension = ($format !== '' ? ".{$format}" : $format);
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/ResponseListenerBundle/hello/ResponseListenerBundle' . $extension);
+        $this->assertEquals($status_code, $client->getResponse()->getStatusCode(), 'ステータスコードチェック');
+        $this->assertEquals($content_type, $client->getResponse()->headers->get('Content-Type'), 'Content-Typeチェック');
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        if ($status_code === 200) {
+            $extension = ($format !== '' ? ".{$format}" : ".html");
+            $this->assertEquals(file_get_contents(__DIR__ . "/DefaultControllerTest_result{$extension}"), $client->getResponse()->getContent());
+        }
     }
 }
