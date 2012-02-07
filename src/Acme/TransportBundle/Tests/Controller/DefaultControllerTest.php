529b062f35e782e216647d21d9247a1e1d880862
Hideyuki Shimooka <shimooka@doyouphp.jp>
2012-02-07 14:26:30 +0900

    - fixed test

--- src/Acme/TransportBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Acme/TransportBundle/Tests/Controller/DefaultControllerTest.php
@@ -9,9 +9,8 @@ class DefaultControllerTest extends WebTestCase
     public function testIndex()
     {
         $client = static::createClient();
-
-        $crawler = $client->request('GET', '/hello/Fabien');
-
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $crawler = $client->request('GET', '/TransportBundle/');
+        $this->assertTrue($crawler->filter('html:contains("Swift_NullTransport")')->count() === 1);
+        $this->assertTrue($crawler->filter('html:contains("Swift_Transport_NullTransport")')->count() === 1);
     }
 }
